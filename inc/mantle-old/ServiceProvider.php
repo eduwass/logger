f0f8898749fd8447238208cd1a0f0ee6b311bf39
Sean Fisher <srtfisher@gmail.com>
2020-05-20 22:08:20 -0400

    Move service provider

--- inc/mantle-old/ServiceProvider.php
+++ inc/mantle-old/ServiceProvider.php
@@ -10,13 +10,13 @@ class Service_Provider extends \Mantle\Framework\Service_Provider {
 	 * Register any application services.
 	 */
 	public function register() {
-		var_dump( 'logger register', $this->app ?? null );
+		//
 	}
 
 	/**
 	 * Bootstrap services.
 	 */
 	public function boot() {
-		var_dump( 'logger boot', $this->app ?? null );
+		//
 	}
 }
