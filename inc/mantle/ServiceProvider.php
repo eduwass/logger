d71177da0bea34e7957cbd2a5c336d75ea0d8cb4
Sean Fisher <srtfisher@gmail.com>
2020-05-20 21:57:23 -0400

    Fixing composer

--- inc/mantle/ServiceProvider.php
+++ inc/mantle/ServiceProvider.php
@@ -1,6 +1,9 @@
 <?php
 namespace AI_Logger\Mantle;
 
+require_once __DIR__ . '/../autoload.php';
+require_once __DIR__ . '/../bootstrap.php';
+
 class Service_Provider extends \Mantle\Framework\Service_Provider {
 
 	/**
